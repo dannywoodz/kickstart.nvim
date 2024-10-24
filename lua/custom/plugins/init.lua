4ef0457450236009ad7288cda7fabd35102c36f5
Danny Woods <dannywoodz@gmail.com>
2024-10-24 10:41:44 +0100

    Add Tabular

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,13 @@
 -- See the kickstart.nvim README for more information
 vim.opt.shortmess = 'I'
 vim.opt.cursorline = false
+require('nvim-treesitter.configs').setup {
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      node_incremental = 'v',
+      node_decremental = 'V',
+    },
+  },
+}
 return {}
