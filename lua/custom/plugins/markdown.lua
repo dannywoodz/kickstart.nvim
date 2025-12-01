d91c2eb2bc7b8963584c147e4e66ebd9c3650044
Danny Woods <dannywoodz@gmail.com>
2025-12-01 20:15:26 +0000

    markdown plugins

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,14 @@
+return {
+  'sbdchd/neoformat',
+  'ixru/nvim-markdown',
+  'dhruvasagar/vim-open-url',
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+}
